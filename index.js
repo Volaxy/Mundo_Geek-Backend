a388f5fdad49825f0557b5afdf59333300bee3a2
Gustavo Martins Pereira <guga.PRO.00@hotmail.com>
2024-03-01 12:06:26 -0300

    ADD - CORS for the Dev Frontend

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import express, { json } from "express";
+import cors from "cors";
 
 import { routes } from "./routes.js";
 import "./src/db/connection.js";
 
 const app = express();
 app.use(json());
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: "POST,GET",
+}));
 app.use(routes);
 
 const PORT = 3000;
